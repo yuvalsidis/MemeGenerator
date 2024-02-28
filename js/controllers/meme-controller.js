b662ae78553539033970b430111f4a4db8c95494
yuvalsidis <yuvalsidis@gmail.com>
2024-02-28 15:22:18 +0200

    change name of img folders

--- js/controllers/meme-controller.js
+++ js/controllers/meme-controller.js
@@ -3,8 +3,13 @@ let gElCavnas
 let gCtx
 
 function onInit() {
-    gElCavnas = document.querySelector('main-content gallery main-canvas')
+    gElCavnas = document.querySelector('.main-content .gallery .main-canvas')
     gCtx = gElCavnas.getContext('2d')
     console.log(gElCavnas)
     console.log(gCtx)
+}
+
+function renderImage() {
+    const img = new Image()
+    img.src = 'img/meme-imgs(square)'
 }
