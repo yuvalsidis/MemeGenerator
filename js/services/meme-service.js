ff6ee34849ba2c602f2cacd40566440a3391ac8b
yuvalsidis <yuvalsidis@gmail.com>
2024-02-29 18:39:53 +0200

    Create function createImgs

--- js/services/meme-service.js
+++ js/services/meme-service.js
@@ -1,30 +1,34 @@
-var gImgs = [{id: 1, url: 'img/meme-imgs-square/1.jpg', keywords: ['funny', 'cat']}]
-var gMeme = {
- selectedImgId: 1,
- selectedLineIdx: 0,
- lines: [
- {
- txt: 'Insert ext here',
- size: 40,
- color: 'red'
- }
- ]
-}
-var gKeywordSearchCountMap = {'funny': 12,'cat': 16, 'baby': 2}
 
-function getMeme(){
-   return gMeme
+var gImgs = [{ id: 1, url: 'img/meme-imgs-square/1.jpg', keywords: ['funny', 'cat'] }]
+var gMeme = {
+    selectedImgId: 1,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: 'Insert ext here',
+            size: 40,
+            color: 'red'
+        }
+    ]
+}
+
+_createImgs()
+console.log(gImgs)
+var gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
+
+function getMeme() {
+    return gMeme
 }
 
 function getSelectedImg() {
-    return gImgs.find((img) => img.id === gMeme.selectedImgId)   
+    return gImgs.find((img) => img.id === gMeme.selectedImgId)
 }
 
-function getImg(){
-    return gImgs 
+function getImg() {
+    return gImgs
 }
 
-function getLine(){
+function getLine() {
     return gMeme.lines[gMeme.selectedLineIdx]
 }
 
@@ -32,3 +36,13 @@ function setLineText(text) {
     gMeme.lines[gMeme.selectedLineIdx].txt = text
 }
 
+function _createImgs() {
+    for (var i = 1; i <= 45; i++)
+        gImgs.push(
+            {
+                id: i,
+                url: `img/meme-imgs-square${i}.jpg`, 
+                keywords: ['funny', 'cat'] //Fix Keywords for every Image
+            }
+        )
+}
