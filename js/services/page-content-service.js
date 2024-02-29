bd57d0ed730cf054bdda1021fe7244243120cef0
yuvalsidis <yuvalsidis@gmail.com>
2024-02-29 19:12:17 +0200

    Create init functions to remove content when clicking btns

--- js/services/page-content-service.js
+++ js/services/page-content-service.js
@@ -1,5 +1,13 @@
 // THIS FILE HANDLE THE GALLERY AND EDITOR CONTENT
 // REMOVE AND ADD THE CONTENT
 
+let gMainContent = document.querySelector('.main-content')
 
 
+function onClickGallery(){
+    gMainContent.innerHTML = ''
+}
+
+function onClickEditor() {
+    gMainContent.innerHTML = ''
+}
