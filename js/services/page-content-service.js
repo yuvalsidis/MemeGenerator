aff9f4d19388d906b1cbe296a7ab23e3afe4fa62
yuvalsidis <yuvalsidis@gmail.com>
2024-03-01 13:53:06 +0200

    Modify onclickgallerty and clickeditor render

--- js/services/page-content-service.js
+++ js/services/page-content-service.js
@@ -1,14 +1,22 @@
+'use strict'
+
 // THIS FILE HANDLE THE GALLERY AND EDITOR CONTENT
 // REMOVE AND ADD THE CONTENT
 
-let gMainContent = document.querySelector('.main-content')
 
+var editorContent = document.querySelector('.main-content').innerHTML
 
 function onClickGallery(){
-    gMainContent.innerHTML = ''
+    onSaveCanvas()
     renderGallery()
 }
 
 function onClickEditor() {
-    gMainContent.innerHTML = ''
+    const mainContent = document.querySelector('.main-content')
+    mainContent.innerHTML = editorContent
+    onInit()
+    onLoadFromCavnas() 
 }
+
+
+
