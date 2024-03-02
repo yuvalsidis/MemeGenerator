7bd2ea29e515eefe86e4c528d163e6b61e59c259
yuvalsidis <yuvalsidis@gmail.com>
2024-03-02 23:18:32 +0200

    change gallery contoller to make image with 200px

--- js/controllers/gallery-controller.js
+++ js/controllers/gallery-controller.js
@@ -5,7 +5,7 @@ function renderGallery() {
     let gMainContent = document.querySelector('.main-content')
     let strHTML = []
     gGalleryImgs.map((img) => {
-        strHTML.push(`<img src="${img.url}" onclick="onSelectImg(${img.id})">`)
+        strHTML.push(`<img src="${img.url}" onclick="onSelectImg(${img.id})" style="height: 200px; width: auto;">`)
     })
     strHTML = strHTML.join('')
     gMainContent.innerHTML = strHTML
