f883ad1c7d7d21b45c810e8a1c45e9b7973ab858
yuvalsidis <yuvalsidis@gmail.com>
2024-03-02 22:41:57 +0200

    Add toggleMenu functions that work as expected

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 //  menu open
-function toggleMenu(){
-    document.body.classList.toggle('menu-open')
+
+function toggleMenu() {
+    const elmHeaderButtons = document.querySelector('.main-header .header-btns');
+    const elmToggleButtons = document.querySelector('.main-layout .toggle-menu-btn');
+    const isMenuOpen = document.body.classList.toggle('menu-open');
+    if (isMenuOpen) {
+        elmHeaderButtons.style.transform = "translateX(0)";
+        elmToggleButtons.style.display = 'none';
+    } else {
+        elmHeaderButtons.style.transform = "translateX(100%)";
+        elmToggleButtons.style.display = 'block';
+    }
 }
